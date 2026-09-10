e98d19468a2448aba42730c8efe7f0f9c2e25fdf
SeeItFirstCreative <danilo@seeitfirstcreative.com>
2026-09-09 23:08:24 -0400

    Add application layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Utecht Growth Intelligence',description:'Commercial P&C Growth Command Center'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
